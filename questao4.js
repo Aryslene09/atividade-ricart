631b8735d66880215979015919b25b9afff4641b
Aryslene09 <146980766+Aryslene09@users.noreply.github.com>
2024-10-23 16:48:24 -0300

    atualizando questao4.js

--- questao4.js
+++ questao4.js
@@ -1,9 +1,9 @@
 function verificaNumeros(num1, num2) {
-    // Retorna true se um dos números for 30 ou se a soma dos dois for 30
+    
     return num1 === 30 || num2 === 30 || (num1 + num2) === 30;
 }
 
-// Testando a função
+
 const numero1 = parseFloat(prompt("Digite o primeiro número:"));
 const numero2 = parseFloat(prompt("Digite o segundo número:"));
 
