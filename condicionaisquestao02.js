2b730e5915f5578d89c1273ffbf1f0969725e37c
Arianne <arianne.vieira@academico.ifpb.edu.br>
2024-10-28 11:54:08 -0300

    adicionar questao

---
+++ condicionaisquestao02.js
@@ -0,0 +1,11 @@
+function verificarParidade() {
+    for (let i = 0; i <= 50; i++) {
+        if (i % 2 === 0) {
+            console.log(`${i} é par`);
+        } else {
+            console.log(`${i} é ímpar`);
+        }
+    }
+}
+
+verificarParidade();
